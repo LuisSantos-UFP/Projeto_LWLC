a575f0f80572eee4d720ce42370f63b99cf89ffc
Luis <2025111261@ufp.edu.pt>
2026-05-20 21:48:06 +0100

    update html

--- script.js
+++
@@ -1,6 +0,0 @@
-function switchView(viewId) {
-            document.querySelectorAll('.view-section').forEach(section => {
-                section.classList.remove('active');
-            });
-            document.getElementById('view-' + viewId).classList.add('active');
-        }
